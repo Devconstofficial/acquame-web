4b1c97b71cca00d39afbc6197c2a3c42ce60bf28
unknown <razakumail76@gmail.com>
2025-01-23 19:46:32 +0500

    Login Screen Issue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-
 import React from 'react';
-import {
-  Route,
-  Routes, BrowserRouter
-} from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Login from './pages/auth/login';
 import ForgotPassword from './pages/auth/forgot_password';
 import OTPVerification from './pages/auth/otp_verification';
@@ -18,28 +14,30 @@ import Asset from "./pages/asset_page";
 import Mental from "./pages/mental";
 import Profile from "./pages/profile";
 import Layout from './components/layout';
+
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-    <Route path='/' element={<Login />} />
-    <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Routes>
+        {/* Authentication Routes */}
+        <Route path='/' element={<Login />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
         <Route path='/otp-verification' element={<OTPVerification />} />
         <Route path='/reset-password' element={<ResetPassword />} />
-    </Routes>
-      <Layout>
-      <Routes>
-        <Route path='/home' element={<Home />} />
-        <Route path='/physical' element={<Physical />} />
-        <Route path='/financial' element={<Financial />} />
-        <Route path='/medical-screen' element={<Medical />} />
-        <Route path='/weight-screen' element={<Weight />} />
-        <Route path='/height-screen' element={<Height />} />
-        <Route path='/asset-screen' element={<Asset />} />
-        <Route path='/mental' element={<Mental />} />
-        <Route path='/profile' element={<Profile />} />
+
+        {/* Protected Routes with Layout */}
+        <Route element={<Layout />}>
+          <Route path='/home' element={<Home />} />
+          <Route path='/physical' element={<Physical />} />
+          <Route path='/financial' element={<Financial />} />
+          <Route path='/medical-screen' element={<Medical />} />
+          <Route path='/weight-screen' element={<Weight />} />
+          <Route path='/height-screen' element={<Height />} />
+          <Route path='/asset-screen' element={<Asset />} />
+          <Route path='/mental' element={<Mental />} />
+          <Route path='/profile' element={<Profile />} />
+        </Route>
       </Routes>
-      </Layout>
     </BrowserRouter>
   );
 }
